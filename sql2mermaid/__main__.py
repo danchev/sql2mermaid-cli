fe2eba65605d0880e74dafb6611a2d70be8aa695
D. Danchev <12420863+danchev@users.noreply.github.com>
2025-12-15 17:57:15 -0600

    style(main): format code for consistency and readability
    * Adjust import order
    * Standardize string quotes
    * Improve function signature annotation

--- sql2mermaid/__main__.py
+++ sql2mermaid/__main__.py
@@ -1,16 +1,20 @@
-import sys
 import argparse
+import sys
+
 from sql2mermaid import convert
 
-def main():
+
+def main() -> None:
     parser = argparse.ArgumentParser(description="Convert SQL to Mermaid ERD.")
-    parser.add_argument('file', nargs='?', metavar='FILE', help="SQL file to convert (defaults to stdin)")
-    parser.add_argument('--root', type=str, default='root', help="Name of the root table (default: 'root')")
-    parser.add_argument('--display-join', choices=['none', 'upper', 'lower'], default='none', help="Display join type (default: 'none')")
+    parser.add_argument("file", nargs="?", metavar="FILE", help="SQL file to convert (defaults to stdin)")
+    parser.add_argument("--root", type=str, default="root", help="Name of the root table (default: 'root')")
+    parser.add_argument(
+        "--display-join", choices=["none", "upper", "lower"], default="none", help="Display join type (default: 'none')"
+    )
     args = parser.parse_args()
     try:
         if args.file:
-            with open(args.file, 'r') as f:
+            with open(args.file, "r") as f:
                 query = f.read()
         else:
             query = sys.stdin.read()
@@ -19,6 +23,7 @@ def main():
     except Exception as e:
         print(f"Error: {e}", file=sys.stderr)
         sys.exit(1)
+
 
 if __name__ == "__main__":
     main()
